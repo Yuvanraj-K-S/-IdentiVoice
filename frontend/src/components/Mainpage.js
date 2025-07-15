5d2f41187dd9a8396ac7a8174677f0cf46e58439
dhaanish <dhaanish@example.com>
2025-07-15 14:02:56 +0530

    mainpage added

---
+++ frontend/src/components/Mainpage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Mainpage = () => {
+    return (
+        <div>
+            <h1>Welcome to VoicePass</h1>
+            <p>Please authenticate using your voice.</p>
+        </div>
+    );
+};
+export default Mainpage;
